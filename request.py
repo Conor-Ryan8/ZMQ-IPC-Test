a4d99e1e2686999ca6284714b0db303c3202952e
Conor <37616617+Conor-Ryan8@users.noreply.github.com>
2022-06-12 13:55:14 +0100

    Create request.py

---
+++ request.py
@@ -0,0 +1,15 @@
+import zmq
+import time
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("ipc://temp")
+
+while True:
+
+  request = "REQUEST"
+  socket.send(request.encode())
+  print("Sent:", request)
+  response = socket.recv()
+  print("Recieved:", response.decode(),"\n")
+  time.sleep(1)
